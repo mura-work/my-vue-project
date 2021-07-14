1b109aa92f1a59498514954d3d75a0ae5f4c8686
murata-naoya <murata.small.baseball@gmail.com>
2021-07-14 13:37:24 +0000

    firebaseの設定を付与

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,52 @@
 <template>
   <div>
-    <h1>Home</h1>
     <button @click="toUsers">Usersのページに行く</button>
+    <h1>Home</h1>
+    <h3>投稿</h3>
+    <label for="name">名前</label>
+    <input type="text" id="name" v-model="name"><br>
+    <label for="comment">コメント</label>
+    <textarea id="comment" v-model="comment"></textarea>
+    <button @click="createComment">送信</button>
+    <div>一覧</div>
+    <div></div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  data() {
+    return {
+      name: "",
+      comment: "",
+    }
+  },
   methods: {
     toUsers() {
       this.$router.push('users');
+    },
+    createComment() {
+      const API_KEY = process.env.VUE_APP_FIREBASE_DB
+      const url = "https://firestore.googleapis.com/v1/projects/" + API_KEY + "/databases/(default)/documents/comments"
+      axios.post(url,
+      {
+        fields: {
+          name: {
+            stringValue: this.name
+          },
+          comment: {
+            stringValue: this.comment
+          }
+        }
+      }).then(response => {
+        console.log(response);
+      }).catch(error => {
+        console.log(error);
+      });
+      this.name = '';
+      this.comment = '';
     }
   }
 }
